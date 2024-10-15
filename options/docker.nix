0c4cc5a921c3bc0923a933763a1d38a17e40fc92
FilArt <filart97@gmail.com>
2024-10-15 15:28:36 +0200

    move to new ssd

--- options/docker.nix
+++ options/docker.nix
@@ -2,10 +2,11 @@
   virtualisation.docker = {
     enable = true;
     enableOnBoot = false;
-    storageDriver = "btrfs";
+    #storageDriver = "btrfs";
     daemon.settings = {
       userland-proxy = false;
-      data-root = "/home/art/mnt/hdd/.docker_data";
+      #data-root = "/home/art/mnt/hdd/.docker_data";
+      # data-root = "/home/art/mnt/ssd/Users/filar/.docker_data";
     };
   };
 }
--- options/hardware-configuration.nix
+++ options/hardware-configuration.nix
@@ -11,12 +11,12 @@
 
   fileSystems = {
     "/" = {
-      device = "/dev/disk/by-uuid/fa529b92-2db0-41d9-b446-0e6caea97a4b";
+      device = "/dev/disk/by-uuid/373cf0d1-d08c-4fdc-93af-33b6e0c7f2c0";
       fsType = "ext4";
     };
 
     "/boot" = {
-      device = "/dev/disk/by-uuid/28DE-F378";
+      device = "/dev/disk/by-uuid/3F09-D6A9";
       fsType = "vfat";
       options = [ "fmask=0022" "dmask=0022" ];
     };
