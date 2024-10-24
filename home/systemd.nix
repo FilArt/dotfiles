6be833ef9a1d584833eac0aea8e41764c2b2db50
FilArt <filart97@gmail.com>
2024-10-24 21:32:38 +0200

    do not duplicate

--- home/systemd.nix
+++ home/systemd.nix
@@ -2,45 +2,30 @@
 let
   mountPath = "${config.home.homeDirectory}/mnt";
   rcloneParams = "--vfs-cache-mode=writes --vfs-cache-max-age=12h";
+  createMountService = name: remote: {
+    Unit = {
+      After = [ "network-online.target" ];
+      Description = "RClone Mount ${name}";
+    };
+    Install.WantedBy = [ "graphical-session.target" ];
+    Service = {
+      ExecStartPre = "${pkgs.coreutils}/bin/mkdir -p ${mountPath}/${name}";
+      ExecStart = "${pkgs.rclone}/bin/rclone mount ${rcloneParams} ${remote}: ${mountPath}/${name}";
+      ExecStop = "fusermount -u ${mountPath}/${name}";
+      Restart = "always";
+      RestartSec = "10s";
+      Environment = [ "PATH=/run/wrappers/bin/:$PATH" ];
+    };
+  };
 in
 {
   systemd.user.targets.tray = {
     Unit = {
       Description = "Home Manager System Tray";
       After = [ "display-manager.service" ];
-      # Requires = [ "graphical-session-pre.target" ];
     };
   };
 
-  systemd.user.services.gdrive_mount = {
-    Unit = {
-      After = [ "network-online.target" ];
-      Description = "RClone Mount Google Drive";
-    };
-    Install.WantedBy = [ "graphical-session.target" ];
-    Service = {
-      ExecStartPre = "${pkgs.coreutils}/bin/mkdir -p ${mountPath}/gdrive";
-      ExecStart = "${pkgs.rclone}/bin/rclone mount ${rcloneParams} gdrive: ${mountPath}/gdrive";
-      ExecStop = "fusermount -u ${mountPath}/gdrive";
-      Restart = "always";
-      RestartSec = "10s";
-      Environment = [ "PATH=/run/wrappers/bin/:$PATH" ];
-    };
-  };
-
-  systemd.user.services.onedrive_mount = {
-    Unit = {
-      After = [ "network-online.target" ];
-      Description = "RClone Mount OneDrive";
-    };
-    Install.WantedBy = [ "graphical-session.target" ];
-    Service = {
-      ExecStartPre = "${pkgs.coreutils}/bin/mkdir -p ${mountPath}/onedrive";
-      ExecStart = "${pkgs.rclone}/bin/rclone mount ${rcloneParams} onedrive: ${mountPath}/onedrive";
-      ExecStop = "fusermount -u ${mountPath}/onedrive";
-      Restart = "always";
-      RestartSec = "10s";
-      Environment = [ "PATH=/run/wrappers/bin/:$PATH" ];
-    };
-  };
+  systemd.user.services.gdrive_mount = createMountService "gdrive" "gdrive";
+  systemd.user.services.onedrive_mount = createMountService "onedrive" "onedrive";
 }
