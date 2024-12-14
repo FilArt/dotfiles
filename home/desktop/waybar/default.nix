8be43f025367eca9d89eb590d8834697c3685abc
FilArt <filart97@gmail.com>
2024-12-14 11:11:36 +0100

    use systemd waybar

--- home/desktop/waybar/default.nix
+++ home/desktop/waybar/default.nix
@@ -1,52 +1,6 @@
 { config, ... }: {
-  programs.waybar = {
-    enable = true;
-    # settings = {
-    #   mainBar = {
-    #     height = 34;
-    #     modules-left = [ "hyprland/workspaces" ];
-    #     modules-center = [ "hyprland/window" ];
-    #     modules-right = [
-    #       "hyprland/language"
-    #       "systemd-failed-units"
-    #       "wireplumber"
-    #       "pulseaudio#microphone"
-    #       "memory"
-    #       "disk"
-    #       "battery"
-    #       "clock"
-    #       "tray"
-    #       "custom/notification"
-    #       "custom/lock"
-    #     ];
-
-    #     include = [ "${./waybar.json}" ];
-
-    #     "custom/notification" = {
-    #       tooltip = false;
-    #       format = "{} {icon}";
-    #       "format-icons" = {
-    #         notification = "󱅫 ";
-    #         none = " ";
-    #         "dnd-notification" = "";
-    #         "dnd-none" = "󰂛";
-    #         "inhibited-notification" = "";
-    #         "inhibited-none" = "";
-    #         "dnd-inhibited-notification" = "";
-    #         "dnd-inhibited-none" = "";
-    #       };
-    #       "return-type" = "json";
-    #       "exec-if" = "which swaync-client";
-    #       exec = "swaync-client -swb";
-    #       "on-click" = "sleep 0.1 && swaync-client -t -sw";
-    #       "on-click-right" = "sleep 0.1 && swaync-client -d -sw";
-    #       escape = true;
-    #     };
-    #   };
-    # };
-
-  };
-
+  programs.waybar.systemd.enable = true;
+  programs.waybar.enable = true;
   home.file.".config/waybar/config.jsonc".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/home-manager/home/desktop/waybar/config.jsonc";
   home.file.".config/waybar/style.css".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/home-manager/home/desktop/waybar/style.css";
 }
