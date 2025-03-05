66e52fafbfe80b0dd9a98a35331bf7eec26e6a0e
FilArt <filart97@gmail.com>
2025-03-05 23:22:28 +0100

    add solaar

--- flake.nix
+++ flake.nix
@@ -13,32 +13,40 @@
       url = "github:nix-community/nixvim";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    solaar = {
+      url = "https://flakehub.com/f/Svenum/Solaar-Flake/*.tar.gz"; # For latest stable version
+      #url = "https://flakehub.com/f/Svenum/Solaar-Flake/0.1.1.tar.gz"; # uncomment line for solaar version 1.1.13
+      #url = "github:Svenum/Solaar-Flake/main"; # Uncomment line for latest unstable version
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs =
-    inputs @ { nixpkgs
-    , home-manager
-    , hyprland
-    , nixvim
-    , ...
-    }: {
-      nixosConfigurations.art = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = {
-          inherit inputs;
-        };
-        modules = [
-          ./configuration.nix
-
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.art = import ./home.nix;
-          }
-
-          nixvim.nixosModules.nixvim
-        ];
+  outputs = inputs @ {
+    nixpkgs,
+    home-manager,
+    hyprland,
+    nixvim,
+    solaar,
+    ...
+  }: {
+    nixosConfigurations.art = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = {
+        inherit inputs;
       };
+      modules = [
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.users.art = import ./home.nix;
+        }
+
+        nixvim.nixosModules.nixvim
+
+        solaar.nixosModules.solaar
+
+        ./configuration.nix
+      ];
     };
+  };
 }
