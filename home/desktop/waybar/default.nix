715672c0f16a71599a7c901ebc3fb7b83ef90c98
Artur Filimonov <filart97@gmail.com>
2025-05-03 23:07:19 +0200

    upd waybar

--- home/desktop/waybar/default.nix
+++ home/desktop/waybar/default.nix
@@ -2,9 +2,9 @@
   programs.waybar = {
     enable = true;
     systemd = {
-      enable = true;
+      enable = false;
     };
   };
   home.file.".config/waybar/config.jsonc".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/home-manager/home/desktop/waybar/config.jsonc";
-  # home.file.".config/waybar/style.css".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/home-manager/home/desktop/waybar/style.css";
+  home.file.".config/waybar/style.css".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/home-manager/home/desktop/waybar/style.css";
 }
