5975477c99b553222c77dd5f8ef87d69f5c9db4b
Artur Filimonov <filart97@gmail.com>
2025-10-23 08:46:27 +0200

    update zellij

--- home/programs/zellij.nix
+++ home/programs/zellij.nix
@@ -4,6 +4,12 @@
     enableZshIntegration = true;
     settings = {
       show_startup_tips = false;
+      keybinds = {
+        unbind = [
+          "Alt j"
+          "Alt k"
+        ];
+      };
     };
   };
 }
