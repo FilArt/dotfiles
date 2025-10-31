e0beaf69c9f8a5ef6b8db340d8f51b7562d35a3a
Artur Filimonov <filart97@gmail.com>
2025-10-31 18:05:34 +0100

    decrease noty volume

--- home/services/mako.nix
+++ home/services/mako.nix
@@ -2,7 +2,7 @@
   services.mako = {
     enable = true;
     settings = {
-      on-notify = "exec ${pkgs.mpv}/bin/mpv $HOME/Music/noty.wav";
+      on-notify = "exec ${pkgs.mpv}/bin/mpv --volume=50 $HOME/Music/noty.wav";
     };
   };
 }
