6acaa6f5123d3843e07f74c1d27729522403148e
Artur Filimonov <filart97@gmail.com>
2025-10-31 18:05:24 +0100

    unbind ctrl-o

--- home/programs/zellij.nix
+++ home/programs/zellij.nix
@@ -5,10 +5,14 @@
     settings = {
       show_startup_tips = false;
       keybinds = {
+        "shared_except \"session\" \"locked\"" = {
+          "bind \"Ctrl i\"".SwitchToMode = "Session";
+        };
         unbind = [
           "Alt j"
           "Alt k"
           "Alt o"
+          "Ctrl o"
         ];
       };
     };
