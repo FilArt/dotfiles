0095c1a5061253e5e31e4d28b121530249c82a5d
Artur Filimonov <filart97@gmail.com>
2025-12-14 13:08:07 +0100

    update xdg.nix

--- home/xdg.nix
+++ home/xdg.nix
@@ -3,24 +3,16 @@
   pkgs,
   ...
 }: let
-  gamesDir = "${config.home.homeDirectory}/Games";
-  runner = "${gamesDir}/runner.sh";
-  steam-run = "${pkgs.steam}/bin/steam-run";
-  gamemoderun = "${pkgs.steam}/bin/gamemoderun";
-  # gamescope = "${pkgs.steam}/bin/gamescope";
-  # launchArgs = "${steam-run} ${gamemoderun} ${gamescope} --prefer-output HDMI-A-1 --output-width 1920 --output-height 1080 	--nested-width 1920 --nested-height 1080 	--adaptive-sync 	 ${@}";
+  homeDir = config.home.homeDirectory;
+  gamesDir = "${homeDir}/Games";
+  steam-run = "${pkgs.steam-run}/bin/steam-run";
+  gamemoderun = "${pkgs.gamemode}/bin/gamemoderun";
+  gamescope = "${pkgs.gamescope}/bin/gamescope";
+  qhd = ''--output-width "3840" --output-height "2160" --nested-width "3840" --nested-height "2160"'';
+  fullHD = ''--output-width "1920" --output-height "1080" --nested-width "1920" --nested-height "1080"'';
+  runGame = ''${steam-run} ${gamemoderun} ${gamescope} ${qhd} --adaptive-sync --hdr-enabled --rt --steam'';
 in {
   xdg.enable = true;
   xdg.userDirs.enable = true;
   xdg.userDirs.createDirectories = true;
-
-  xdg.desktopEntries = {
-    # tw_wh2 = {
-    #   name = "Total War: Warhammer 2";
-    #   exec = "env -u SDL_VIDEODRIVER ${runner} ${gamesDir}/total-war-warhammer-2/game/start.sh";
-    #   terminal = false;
-    #   type = "Application";
-    #   categories = [ "Game" ];
-    # };
-  };
 }
