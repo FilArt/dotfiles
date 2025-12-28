54cdb6ca9aea0eac216189366da9985f84d27589
Artur Filimonov <filart97@gmail.com>
2025-12-28 21:26:21 +0100

    fix plugins

--- home.nix
+++ home.nix
@@ -75,22 +75,18 @@
       enableKeybinds = true; # Sets static preset keybinds
       enableSpawn = true; # Auto-start DMS with niri and cliphist, if enabled
     };
-    # Core features
-    enableSystemMonitoring = true; # System monitoring widgets (dgop)
-    # enableVPN = true; # VPN management widget
-    enableDynamicTheming = true; # Wallpaper-based theming (matugen)
-    enableAudioWavelength = true; # Audio visualizer (cava)
-    # enableCalendarEvents = true; # Calendar integration (khal)
-    # plugins = {
-    #   DockerManager = {
-    #     src = pkgs.fetchFromGitHub {
-    #       owner = "LuckShiba";
-    #       repo = "DmsDockerManager";
-    #       rev = "v1.2.0";
-    #       sha256 = "sha256-VoJCaygWnKpv0s0pqTOmzZnPM922qPDMHk4EPcgVnaU=";
-    #     };
-    #   };
-    # };
+    enableSystemMonitoring = true;
+    enableAudioWavelength = true;
+    plugins = {
+      DockerManager = {
+        src = pkgs.fetchFromGitHub {
+          owner = "LuckShiba";
+          repo = "DmsDockerManager";
+          rev = "v1.2.0";
+          sha256 = "sha256-VoJCaygWnKpv0s0pqTOmzZnPM922qPDMHk4EPcgVnaU=";
+        };
+      };
+    };
   };
 
   home = {
