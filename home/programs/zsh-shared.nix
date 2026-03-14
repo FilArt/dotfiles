6490105b8d5aaa9d8e584af88a38bcb99acf56e3
Artur Filimonov <filart97@gmail.com>
2026-03-14 14:02:32 +0100

    share starship

--- home/programs/zsh-shared.nix
+++ home/programs/zsh-shared.nix
@@ -10,4 +10,26 @@
       path = "${config.xdg.dataHome}/zsh/history";
     };
   };
+
+  programs.starship = {
+    enable = true;
+    settings = {
+      username = {
+        style_user = "blue bold";
+        style_root = "red bold";
+        format = "[$user]($style) ";
+        disabled = false;
+        show_always = true;
+      };
+      add_newline = true;
+      aws.disabled = true;
+      gcloud.disabled = true;
+      line_break.disabled = false;
+      scan_timeout = 10;
+      command_timeout = 100;
+      dotnet.disabled = true;
+      nodejs.disabled = true;
+      package.disabled = true;
+    };
+  };
 }
--- home/programs/zsh.nix
+++ home/programs/zsh.nix
@@ -10,31 +10,8 @@
 
   programs.zsh.shellAliases = {
     switch = "nh os switch";
-    archie-switch = "nh os switch . -H archie --target-host archie";
+    archie-switch = "nh os switch ~/.config/home-manager -H archie --target-host archie";
     df = lib.getExe pkgs.duf;
     duf = lib.getExe pkgs.duf;
-  };
-
-  programs.starship = {
-    enable = true;
-    settings = {
-      username = {
-        style_user = "blue bold";
-        style_root = "red bold";
-        format = "[$user]($style) ";
-        disabled = false;
-        show_always = true;
-      };
-      hostname.disabled = true;
-      add_newline = true;
-      aws.disabled = true;
-      gcloud.disabled = true;
-      line_break.disabled = false;
-      scan_timeout = 10;
-      command_timeout = 100;
-      dotnet.disabled = true;
-      nodejs.disabled = true;
-      package.disabled = true;
-    };
   };
 }
