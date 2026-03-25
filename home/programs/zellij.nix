af0b448b630b6bb10703c221c2b9e18133740662
Artur Filimonov <filart97@gmail.com>
2026-03-25 10:56:15 +0100

    add zellij layout for cv

--- home/programs/zellij.nix
+++ home/programs/zellij.nix
@@ -16,5 +16,112 @@
         ];
       };
     };
+
+    layouts = {
+      gg = {
+        layout = {
+          _props = {
+            cwd = "~/Projects/call-visit";
+          };
+          _children = [
+            {
+              default_tab_template = {
+                _children = [
+                  {
+                    pane = {
+                      size = 1;
+                      borderless = true;
+                      plugin = {
+                        location = "zellij:tab-bar";
+                      };
+                    };
+                  }
+                  {children = {};}
+                  # {
+                  #   pane = {
+                  #     size = 2;
+                  #     borderless = true;
+                  #     plugin = {
+                  #       location = "zellij:status-bar";
+                  #     };
+                  #   };
+                  # }
+                ];
+              };
+            }
+
+            {
+              tab = {
+                _props = {
+                  name = "Project";
+                  focus = true;
+                  split_direction = "vertical";
+                };
+                _children = [
+                  {
+                    pane = {
+                      command = "devenv";
+                      args = ["shell" "--" "uv" "run" "hx"];
+                      borderless = true;
+                      size = "60%";
+                    };
+                  }
+                  {
+                    pane = {};
+                  }
+                ];
+              };
+            }
+
+            {
+              tab = {
+                _props = {
+                  name = "Codex";
+                };
+                _children = [
+                  {
+                    pane = {
+                      command = "codex";
+                    };
+                  }
+                ];
+              };
+            }
+
+            {
+              tab = {
+                _props = {
+                  name = "Git";
+                };
+                _children = [
+                  {
+                    pane = {
+                      command = "lazygit";
+                    };
+                  }
+                ];
+              };
+            }
+
+            {
+              tab = {
+                _props = {
+                  name = "Processes";
+                };
+                _children = [
+                  {
+                    pane = {
+                      command = "devenv";
+                      args = ["up"];
+                      start_suspended = true;
+                    };
+                  }
+                ];
+              };
+            }
+          ];
+        };
+      };
+    };
   };
 }
