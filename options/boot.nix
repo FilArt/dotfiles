ec30df7958766415fac14f82843cc8148dcb18de
Artur Filimonov <filart97@gmail.com>
2026-03-25 10:56:19 +0100

    update boot

--- options/boot.nix
+++ options/boot.nix
@@ -1,11 +1,11 @@
 {pkgs, ...}: {
   boot = {
-    loader.timeout = 15;
     initrd = {
       verbose = false;
-      availableKernelModules = ["xhci_pci" "thunderbolt" "nvme" "usb_storage" "uas" "sd_mod"];
       kernelModules = ["amdgpu"];
     };
+    kernelPackages = pkgs.linuxKernel.packages.linux_xanmod;
+    # kernelPackages = pkgs.linuxPackages_latest;
     # kernelPackages = pkgs.linuxKernel.packages.linux_zen;
     kernel.sysctl = {
       "kernel.sysrq" = 1;
@@ -14,21 +14,15 @@
       "mitigations=off"
       "nowatchdog"
       "nmi_watchdog=0"
-      "usbcore.autosuspend=120" # 2 minutes
       "amd_pstate=active"
     ];
-    # blacklistedKernelModules = ["iTCO_wdt" "rtw88_8822bu"];
-    # kernelModules = ["88x2bu"];
-    # extraModulePackages = [pkgs.linuxKernel.packages.linux_zen.rtl88x2bu];
-    # extraModprobeConfig = ''
-    #   options 88x2bu rtw_switch_usb_mode=1
-    # '';
     tmp = {
       cleanOnBoot = true;
       useTmpfs = true;
     };
-    consoleLogLevel = 3;
+    consoleLogLevel = 2;
     loader = {
+      timeout = 5;
       efi.canTouchEfiVariables = true;
       grub = {
         enable = true;
